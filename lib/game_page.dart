83c76cc5a9ef65511d29b7d91980301a0c684d4f
kenji0622s <kenji0622s@gmail.com>
2024-04-15 23:49:31 +0900

    ゲームページの追加

---
+++ lib/game_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class GamePage extends StatefulWidget {
+  const GamePage({super.key});
+
+  @override
+  State<GamePage> createState() => GamePageState();
+}
+
+class GamePageState extends State<GamePage> {
+  final List<int> _items = List<int>.generate(5, (int index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+
+    return Material(
+      child: ReorderableListView(
+        padding: const EdgeInsets.symmetric(horizontal: 40),
+        children: <Widget>[
+          for (int index = 0; index < _items.length; index += 1)
+            ListTile(
+              key: Key('$index'),
+              tileColor: _items[index].isOdd ? oddItemColor : evenItemColor,
+              title: Text('Item ${_items[index]}'),
+            ),
+        ],
+        onReorder: (int oldIndex, int newIndex) {
+          setState(() {
+            if (oldIndex < newIndex) {
+              newIndex -= 1;
+            }
+            final int item = _items.removeAt(oldIndex);
+            _items.insert(newIndex, item);
+          });
+        },
+      ),
+    );
+  }
+}
--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,3 +1,4 @@
+import 'package:chronomap_mobile/game_page.dart';
 import 'package:chronomap_mobile/register_page.dart';
 import 'package:flutter/material.dart';
 import 'search_page.dart';
@@ -55,6 +56,22 @@ class IndexPage extends StatelessWidget {
                     },
                     child: const Text(
                       "Search & View",
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: OutlinedButton(
+                    onPressed: () {
+                      Navigator.push<String>(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const GamePage(),
+                        ),
+                      );
+                    },
+                    child: const Text(
+                      "Game",
                     ),
                   ),
                 ),
