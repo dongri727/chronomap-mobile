56520784fb712356aa59d3df80f6bebde0f13201
laporte727 <ecole.la.porte@gmail.com>
2024-04-18 16:04:31 +0900

    client接続の修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,13 @@ import 'package:acorn_client/acorn_client.dart';
 import 'package:chronomap_mobile/timeline/bloc_provider.dart';
 import 'package:chronomap_mobile/timeline/timeline.dart';
 import 'package:flutter/material.dart';
-import 'package:serverpod_auth_shared_flutter/serverpod_auth_shared_flutter.dart';
-import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 import 'index_page.dart';
-
-late SessionManager sessionManager;
-late Client client;
+import 'seerverpod_client.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  var client = Client(
-    'https://api.laporte.academy/',
-    authenticationKeyManager: FlutterAuthenticationKeyManager(),
-  )..connectivityMonitor = FlutterConnectivityMonitor();
-
-  sessionManager = SessionManager(
-    caller: client.modules.auth,
-  );
-  await sessionManager.initialize();
+  await initializeServerpodClient();
 
   runApp(const MyApp());
 }
