c05b96817d9bc0f4246ee0a27b74ac374681e7a8
kenji0622s <kenji0622s@gmail.com>
2024-05-03 23:10:16 +0900

    splashの表示を2秒間に設定

--- lib/tab.dart
+++ lib/tab.dart
@@ -1,6 +1,7 @@
 import 'package:chronomap_mobile/register/register_page.dart';
 import 'package:chronomap_mobile/scalable/menu/scalable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'game_page.dart';
 import 'index_page.dart';
 import 'search_page.dart';
@@ -29,6 +30,18 @@ class _TabWidgetState extends State<TabWidget> {
     });
   }
 
+  // splash画面を表示時間を調整するために、下のinitstateを追加します。
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      await Future<void>.delayed(const Duration(seconds: 2));
+      setState(() {
+        FlutterNativeSplash.remove();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,11 +54,10 @@ class _TabWidgetState extends State<TabWidget> {
           onTap: _onItemTapped,
           items: const <BottomNavigationBarItem>[
             BottomNavigationBarItem(icon: Icon(Icons.home), label: 'HOME'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.add), label: 'REGISTER'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'REGISTER'),
             BottomNavigationBarItem(icon: Icon(Icons.search), label: 'CLASSIC'),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: 'SCALABLE'),
-
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), label: 'SCALABLE'),
             BottomNavigationBarItem(icon: Icon(Icons.games), label: 'GAME'),
           ],
           type: BottomNavigationBarType.fixed,
